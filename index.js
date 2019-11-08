b1d1afefa1f2296abfd4ff66b849dae9ce86c574
Jeff Schwartz <jefftschwartz@gmail.com>
2019-11-08 15:03:29 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "extends": "standard",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": 0,
+        "eol-last": "off",
+        "no-mixed-operators": "off"
+    }
+};
